1d56c2dc63e1239b571d5dfc965ac00bb8ef75a1
Jonathan Commins <joncom@gmail.com>
2013-07-09 11:07:27 -0700

    Moved documenation into README.

---
+++ README.md
@@ -0,0 +1,22 @@
+##### Impact gotoFrame Fix #####
+Fix for `ig.Animation.gotoFrame` which sometimes
+does not immediately select the argument frame.
+
+### Example Problem ###
+If
+```
+this.frameTime == 0.03, f == 11
+```
+Then
+```
+(0.03 * -11) // is passed to this.timer.set()
+``
+In reality:
+```
+(0.03 * -11) == -0.33
+```
+In JavaScript:
+```
+(0.03 * -11) == -0.32999999999999996
+```
+Therefor we round.
--- animation.js
+++ animation.js
@@ -1,20 +1,3 @@
-/* Fix for animation.gotoFrame(f) which sometimes
- * does not immediately select the argument frame.
- *
- * Example Problem:
- *
- * If
- *      this.frameTime == 0.03, f == 11
- * Then
- *      (0.03 * -11) is passed to this.timer.set()
- *
- * In reality:
- *      (0.03 * -11) == -0.33
- * In JavaScript:
- *      (0.03 * -11) == -0.32999999999999996
- *
- * Therefor we round.
- */
 ig.module('plugins.go-to-frame-fix.animation')
 .requires('impact.animation')
 .defines(function(){ "use strict";
